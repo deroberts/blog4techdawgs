e930c322bbed7f7dc160cc3e0937ab8e6f74e5b4
deroberts <s501656@gmail.com>
2023-02-22 16:55:34 -0600

    added delete partial, but I need to move on with other assignments.

--- controllers/api/reviewroutes.js
+++ controllers/api/reviewroutes.js
@@ -34,5 +34,24 @@ router.post('/comment', async (req, res) => {
   };
   });
 
+  router.delete("/:id", async (req, res) => {
+    try {
+      const reviewData = await Review.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id
+        }
+      });
+      if (!reviewData) {
+        res.status(404).json({ message: "No review found with this id!" });
+        return;
+      }
+      res.status(200).json(reviewData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,14 @@
 const helpers = {
-    toLower: (str) => str.toLowerCase(),
-    toUpper: (str) => str.toUpperCase(),
-    filterUsers: (usersArray) => {
-      return usersArray
-        .filter((user) => user.posts.length)
-        .map((filteredUser) => (filteredUser.active = true));
-    }
-  };
-  
-  module.exports = helpers;
+  toLower: (str) => str.toLowerCase(),
+  toUpper: (str) => str.toUpperCase(),
+  filterUsers: (usersArray) => {
+    return usersArray
+      .filter((user) => user.posts.length)
+      .map((filteredUser) => (filteredUser.active = true));
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getFullYear()}`;
+  },
+};
+
+module.exports = helpers;
